6a73c1ad024cd01aa8164ffc04b00168da114e59
Dmitry Zakharov <dmitry.zakharov.v@outlook.com>
2022-06-15 10:05:57 +1100

    var3

---
+++ variables.tf
@@ -0,0 +1,15 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = ""
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = ""
+}
+
+variable "yc_token" {
+   default = ""
+}
