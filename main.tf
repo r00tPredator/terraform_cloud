1dbf70c8a3b9c68c3095b745a48d17ecc851c9ee
Dmitry Zakharov <dmitry.zakharov.v@outlook.com>
2022-06-15 09:40:40 +1100

    Var

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider yandex {
   #service_account_key_file = "key.json"
-  token  = "${var.token}"
-  cloud_id  = "${var.yandex_cloud_id}"
-  folder_id = "${var.yandex_folder_id}"
+  token  = var.token
+  cloud_id  = var.yandex_cloud_id
+  folder_id = var.yandex_folder_id
   
   zone      = "ru-central1-a"
 }
---
+++ variables.tf
@@ -0,0 +1,11 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1g2hl0o17f780m7bh0d"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1grqk6q3ul251r9fdda"
+}
